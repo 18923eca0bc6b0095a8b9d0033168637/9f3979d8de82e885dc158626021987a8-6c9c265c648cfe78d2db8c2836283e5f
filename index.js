125623070555868486154e5a2b35f10078f072e9
18923eca0bc6b0095a8b9d0033168637 <41907777+18923eca0bc6b0095a8b9d0033168637@users.noreply.github.com>
2018-09-22 12:04:20 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+bot.on('ready', () => {
+    bot.user.setStatus('idle')
+    bot.user.setPresence({
+        game: {
+            name: 'Never online due to school - Coming January, 2019',
+            type: "PLAYING"
+        }
+    });
+});
+
+client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
+
+client.on('reconnecting', () => console.log('I am reconnecting now!'));
+
+client.login(process.env.TOKEN);
